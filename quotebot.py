6e7487d402db2205e7e8c97c1630c9944e0249df
y0sh1 <joshua@theinternets.nl>
2018-05-08 17:02:53 +0300

    updated programma with summary version

--- quotebot.py
+++ quotebot.py
@@ -53,68 +53,21 @@ if not os.path.isfile("quotes.db"):
 QUOTE, AUTHOR, LOCATION = range(3)
 
 PROGRAMMA = {
-    datetime.strptime("20180507", "%Y%m%d"): "Op maandag 7 mei vertrekken wij vanaf 11:30 Leiden Centraal richting "
-                                             "Eindhoven. We vliegen om 16:20 en komen aan om 20:00 in Sofia. Houd "
-                                             "rekening met het tijdsverschil! In Sofia is het namelijk een uurtje "
-                                             "later. Je hoeft niet perse met ons mee te reizen naar Eindhoven. Maar "
-                                             "als je er niet bent wacht het vliegtuig niet. Zorg dus dat je op tijd "
-                                             "bent!\nAls we zijn aangekomen vertrekken wij richting het hostel met de "
-                                             "metro. Na de metro is het nog ongeveer 15 minuten lopen en dan zijn we "
-                                             "er. Zodra iedereen een plekje heeft en zijn spullen heeft weggelegd "
-                                             "hebben jullie vrij voor de rest van de avond om de omgeving te kunnen "
-                                             "verkennen.",
-    datetime.strptime("20180508", "%Y%m%d"): "Rise and shine! Vandaag hebben we een tour door de stad heen. We willen "
-                                             "rond 10:30 verzamelen voor het hostel om vervolgens om 10:45 te "
-                                             "vertrekken naar de tour die rond 11:00 begint. Vergeet je goede "
-                                             "wandelschoenen niet aan te trekken. De tour zal een kleine anderhalf "
-                                             "duren. \nIn de middag kan je aansluiten bij de ReisCo wanneer we naar "
-                                             "het national museum gaan :). Een voorproefje kun je op de site vinden. "
-                                             "https://historymuseum.org/\nIn de avond verzamelen we om 19:30 en gaan "
-                                             "we lekker met elkaar gezellig wat smikkelen*. Dit is elk jaar weer een "
-                                             "groot feest waar we graag iedereen bij zouden zien! Mocht het zo zijn "
-                                             "dat je, om wat voor reden dan ook, hier niet bij aanwezig bent, "
-                                             "dan willen wij dit graag voor aanvang van de studiereis weten. Dit om "
-                                             "te voorkomen dat we te maken krijgen met boze restaurantmedewerkers.\n* "
-                                             "De kosten voor het eten zijn voor eigen rekening, tenzij anders vermeld.",
-    datetime.strptime("20180509", "%Y%m%d"): "Vandaag hebben we in de ochtend en het begin van de middag vrij, "
-                                             "chill! \nIn de ochtend kan je naar een leuk museum gaan met de "
-                                             "reiscommissie, namelijk het socialistische kunst museum.\nIn de middag "
-                                             "kan je eventueel mee naar een groot winkelcentrum, deze zijn door de "
-                                             "hele stad te vinden. \nMocht je dat echt niet willen, zijn er nog "
-                                             "genoeg alternatieven, open je internet en kijk voor wat leuks in de "
-                                             "buurt!\nOm 15:30 verzamelen we weer bij het hostel. Vervolgens is het "
-                                             "tijd voor een leuke activiteit, die tot op locatie geheim blijft!",
-    datetime.strptime("20180510", "%Y%m%d"): "Vandaag hebben we een studiegerichte activiteit voor jullie! Om 11:00 "
-                                             "worden wij verwacht bij Strypes. De planning is om 10:15 te verzamelen "
-                                             "voor het hostel. Als we compleet zijn vertrekken we richting het "
-                                             "bedrijf.\n‘s Middags is er een tweede bedrijvenbezoek gepland. Omdat "
-                                             "deze nog niet helemaal vast staat kunnen wij de hoe en wat nu nog niet "
-                                             "vertellen. Dit krijgen jullie in een later stadium van ons te horen. "
-                                             "Mocht deze activiteit niet doorgaan hebben jullie vrije tijd maar dan "
-                                             "zouden jullie wel aan kunnen sluiten bij de ReisCo die dan een "
-                                             "foodtasting tour gaat doen: http://bit.ly/sofiareis_foodtasting\nIn de "
-                                             "avond is er een pubcrawl gepland en het gaat gegarandeerd een gezellige "
-                                             "avond worden!",
-    datetime.strptime("20180511", "%Y%m%d"): "Wij willen vrijdag graag naar de technische universiteit van Sofia, "
-                                             "echter staat dit nog niet vast en laat de universiteit op zich "
-                                             "wachten.\nMocht dit niet doorgaan dan is deze dag vrij te besteden! Ga "
-                                             "wat leuks doen, huur een fiets, of bezoek een van de vele biertuinen. "
-                                             "Denk ook terug aan de tour, heb je wat leuks gezien waar je nog heen "
-                                             "wilde? Nu is de kans om dat te doen.\nEen aantal leden van de ReisCo "
-                                             "gaan in de middag naar de dierentuin. Om 12:30 staan wij voor het "
-                                             "hostel en vertrekken we naar de dierentuin.",
-    datetime.strptime("20180512", "%Y%m%d"): "Vandaag hebben we bewust vrij gehouden. Nog dingen die je wilde doen? "
-                                             "Nog magneetjes nodig voor op de koelkast? Of nog een leuk cadeau voor "
-                                             "je bae thuis. Vandaag is het de dag om het te kopen. Ook kun je de dag "
-                                             "goed besteden door een bezoek te brengen aan het Rela Monestary! "
-                                             "http://bit.ly/sofiareis_rila\nVoor eventuele vragen of andere dingen "
-                                             "kun je bij de Nederlandse Ambassade terecht, de ReisCo heeft vandaag "
-                                             "vrij!",
-    datetime.strptime("20180513", "%Y%m%d"): "D-Day. Het is 2018, het is een warme zomerdag in Sofia, vandaag vliegen "
-                                             "we weer terug naar het thuisfront. Om 14:05 vertrekt ons vliegtuig en "
-                                             "om 15:45 komen we weer aan in ons geliefde vaderland. Om 11:00 moeten "
-                                             "we uitgecheckt zijn bij het hostel. Vanaf daar is aan ons om op ons "
-                                             "gemak richting het vliegveld te reizen."
+    datetime.strptime("20180508", "%Y%m%d"): "- 10:00 - 15:00 (ish) Tour door Sofia\n"
+                                             "- 15:00 - 18:00 (optioneel) National Museum met de ReisCo\n"
+                                             "- 19:30 verzamelen voor een gezamelijk avondeten bij een hippe tent in de buurt"
+    datetime.strptime("20180509", "%Y%m%d"): "10:00 (optioneel) Museum van Socialistische Kunst met de Reisco\n"
+                                             "15:30 Geheime activiteit: Voor iedereen is al betaald dus wees erbij! Verzamelen voor het hostel\n"
+                                             "20:00 Vrije avond",
+    datetime.strptime("20180510", "%Y%m%d"): "- 10:00 verzamelen voor het hostel om naar Strypes te gaan.\n"
+                                             "Na Strypes gaan we door naar het volgende bedrijf Dreamix. Hier moeten we om 16:00 aanwezig zijn\n"
+                                             "- 18:00 Vrije avond",
+    datetime.strptime("20180511", "%Y%m%d"): "- Vrije ochtend\n"
+                                             "- 11:45 verzamelen bij het hostel om samen naar de universiteit te gaan waar we om 13:00 aanwezig moeten zijn.\n"
+                                             "- 15:00 (ish) naar de dierentuin daar in de buurt\n"
+                                             "- 21:30 (ish, optioneel) Pubcrawl, meld je aan bij de ReisCo voor morgen avond",
+    datetime.strptime("20180512", "%Y%m%d"): "Vrije dag doe waar je zin in hebt...",
+    datetime.strptime("20180513", "%Y%m%d"): "10:00 ingepakt en wel verzamelen bij het hostel we gaan weer naar het vliegtuig."
 
 }
 
